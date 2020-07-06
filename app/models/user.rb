a6b8e54938def88c6e95359bc6a581e9c8dac7bc
Tien Phan <tienphan1989@gmail.com>
2020-07-06 15:48:19 -0400

    User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,19 @@ class User < ApplicationRecord
     has_many :requesters, foreign_key: :client_id, class_name: 'Request'
     has_many :lawyers, through: :requesters 
 
-    #has_secure_password
+    #before save to database, downcase all emails in case two emails are same
+    before_save { self.email = email.downcase } 
+
+    #user only gets made if name less than 50 chars., and present
+    validates :name,  presence: true, length: { maximum: 50 }
+    #set to constant and uses REGEX to filter correct email formats
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    #validates email is valid format and not already used (i.e. unique)
+    validates :email, presence:   true, length: { maximum: 255 },
+                      format:     { with: VALID_EMAIL_REGEX },
+                      uniqueness: { case_sensitive: false }
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 
     def self.lawyers
         User.all.where(lawyer: true)
