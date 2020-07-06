f75c6e9ab8f6b4fd9c8fed2d3d16a89235e57166
Tien Phan <tienphan1989@gmail.com>
2020-07-06 15:25:30 -0400

    Static Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   resources :requests
   resources :users
+
+  #landing page will be home AKA root_path
+  root 'static_pages#home'
+
+  #routes to static help/about/contact page
+  get '/help',    to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
   
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
