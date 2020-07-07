91b899f675c22b3ebdba99f5e9d0f0b36fdd16c9
Tien Phan <tienphan1989@gmail.com>
2020-07-07 15:51:22 -0400

    CSS adds

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,10 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email])
     session[:user_id] = @user.id
     redirect_to user_path(@user.id)
+  # else
+  #   flash.now[:danger] = 'Invalid email/password combination'
+  #   render 'new'
+  # end
   end
 
   def destroy
